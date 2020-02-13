0a7eb603f8b48035f39db4e0790d036889fa9b99
toumb <ioannis.toumboglou@gmail.com>
2020-02-13 14:01:05 +0200

    Entity class

---
+++ src/main/java/com/toumb/movielibrary/entity/Movie.java
@@ -0,0 +1,90 @@
+package com.toumb.movielibrary.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="movies")
+public class Movie {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="title")
+	private String title;
+	
+	@Column(name="category")
+	private String category;
+	
+	@Column(name="year")
+	private int year;
+	
+	@Column(name="description")
+	private String description;
+	
+	@Column(name="image_location")
+	private String imageLocation;
+	
+	public Movie() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImageLocation() {
+		return imageLocation;
+	}
+
+	public void setImageLocation(String imageLocation) {
+		this.imageLocation = imageLocation;
+	}
+
+	@Override
+	public String toString() {
+		return "Movie [id=" + id + ", title=" + title + ", category=" + category + ", year=" + year + ", description="
+				+ description + ", imageLocation=" + imageLocation + "]";
+	}
+
+}
